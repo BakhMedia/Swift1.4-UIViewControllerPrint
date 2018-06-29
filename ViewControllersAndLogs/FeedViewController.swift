01da44c1e16ba7850a45c955c00f53729d96e5c2
Ilya Aleshin <>
2018-06-29 04:04:49 +0300

    init

---
+++ ViewControllersAndLogs/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  ViewControllersAndLogs
+//
+//  Created by Ilya Aleshin on 21.06.2018.
+//  Copyright © 2018 Bakh. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("this view was loaded")
+    }
+    
+}
